b599db9fa193b5166d6f598c8472e30218ce5dea
Aaron-Ekal <ekalaaronkalale@gmail.com>
2022-06-23 17:34:36 +0300

    Deploy to heroku

---
+++ neighbour/static/js/script.js
@@ -0,0 +1,13 @@
+let typed = new Typed(".auto-type", {
+  strings: [
+    "Is A Platform For All Employers and Employees",
+    "Helps You Get a Job Instantly ",
+    "makes it happen!",
+    "Cares for your Growth",
+    "Makes Job Hunting Easier",
+    "Makes Getting qualified People Easier",
+  ],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
